860c460bb8046acbf0174eabb6bc5b7420a394c7
joshua attridge <joshyattridge@joshuas-MacBook-Pro.local>
2023-09-25 21:50:42 +0100

    added liquidity indicator and improved test file.

--- tests/SMC.test.py
+++ tests/SMC.test.py
@@ -1,210 +1,162 @@
 import pandas as pd
 import numpy as np
-from smartmoneyconcepts.smc import smc
 import plotly.graph_objects as go
+import sys
+import os
 
-def test_FVG():
-    df = pd.read_csv('bittrex_btc-usdt.csv')
-    # use the last 200 candles
-    df = df.iloc[-200:]
-    # reset the index
-    df = df.reset_index(drop=True)
+sys.path.append(os.path.abspath("../"))
+from smartmoneyconcepts.smc import smc
 
+df = pd.read_csv("bittrex_btc-usdt.csv")
+df = df.iloc[-200:]
+df = df.reset_index(drop=True)
+fig = go.Figure(
+    data=[
+        go.Candlestick(
+            x=df["date"],
+            open=df["open"],
+            high=df["high"],
+            low=df["low"],
+            close=df["close"],
+        )
+    ]
+)
+
+
+def add_FVG(fig):
     fvg_data = smc.fvg(df)
-
-    fvg = fvg_data["FVG"]
-    direction = fvg_data["Direction"]
-    top = fvg_data["Top"]
-    bottom = fvg_data["Bottom"]
-    size = fvg_data["Size"]
-    mitigated = fvg_data["Mitigated"]
-    mitigated_index = fvg_data["MitigatedIndex"]
-
-    fig = go.Figure(data=[go.Candlestick(x=df['date'],
-                    open=df['open'], high=df['high'],
-                    low=df['low'], close=df['close'])
-                        ])
-    
     # plot a rectangle for each fvg
-    for i in range(len(fvg)):
-        if fvg[i] == 1:
-            if mitigated[i] == 1:
-                fig.add_shape(
-                    # unfilled Rectangle
-                    type="rect",
-                    x0=df['date'][i],
-                    y0=top[i],
-                    x1=df['date'][mitigated_index[i]],
-                    y1=bottom[i],
-                    line=dict(
-                        color=("rgba(255,0,0,0.5)" if direction[i] == 1 else "rgba(0,255,0,0.5)"),
-                        width=1,
-                    ),
-                )
-            else:
-                fig.add_shape(
-                    # unfilled Rectangle
-                    type="rect",
-                    x0=df['date'][i],
-                    y0=top[i],
-                    x1=df['date'][len(df)-1],
-                    y1=bottom[i],
-                    line=dict(
-                        color=("rgba(255,0,0,0.5)" if direction[i] == 1 else "rgba(0,255,0,0.5)"),
-                        width=1,
-                    ),
-                )
-    fig.show()
+    for i in range(len(fvg_data["FVG"])):
+        if fvg_data["FVG"][i] == 1:
+            x1 = (
+                fvg_data["MitigatedIndex"][i]
+                if fvg_data["Mitigated"][i] == 1
+                else len(df) - 1
+            )
+            fig.add_shape(
+                # filled Rectangle
+                type="rect",
+                x0=df["date"][i],
+                y0=fvg_data["Top"][i],
+                x1=df["date"][x1],
+                y1=fvg_data["Bottom"][i],
+                line=dict(
+                    width=0,
+                ),
+                fillcolor="yellow",
+                opacity=0.5,
+            )
+    return fig
 
-test_FVG()
 
-def test_HighsLows():
-    df = pd.read_csv('bittrex_btc-usdt.csv')
-    # use the last 200 candles
-    df = df.iloc[-200:]
-    # reset the index
-    df = df.reset_index(drop=True)
-
+def add_highs_lows(fig):
     highs_lows = smc.highs_lows(df)
-    
-    highs = highs_lows["Highs"]
-    lows = highs_lows["Lows"]
-
-    fig = go.Figure(data=[go.Candlestick(x=df['date'],
-        open=df['open'], high=df['high'],
-        low=df['low'], close=df['close'])
-    ])
 
     # plot a dot on each high and low
-    for i in range(len(highs)):
-        if highs[i] == 1:
-            fig.add_trace(go.Scatter(
-                x=[df['date'][i]],
-                y=[df['high'][i]],
-                mode="markers",
-                marker=dict(
-                    size=10,
-                    color="rgba(255,0,0,1)"
+    for i in range(len(highs_lows["Highs"])):
+        if highs_lows["Highs"][i] == 1 or highs_lows["Lows"][i] == 1:
+            high_or_low = "high" if highs_lows["Highs"][i] == 1 else "low"
+            fig.add_trace(
+                go.Scatter(
+                    x=[df["date"][i]],
+                    y=[df[high_or_low][i]],
+                    mode="markers",
+                    marker=dict(size=10, color="blue", opacity=0.5),
                 )
-            ))
-        if lows[i] == 1:
-            fig.add_trace(go.Scatter(
-                x=[df['date'][i]],
-                y=[df['low'][i]],
-                mode="markers",
-                marker=dict(
-                    size=10,
-                    color="rgba(0,255,0,1)"
-                )
-            ))
-    fig.show()
+            )
+    return fig
 
-test_HighsLows()
 
-def test_BOS():
-    df = pd.read_csv('bittrex_btc-usdt.csv')
-    # use the last 200 candles
-    df = df.iloc[-200:]
-    # reset the index
-    df = df.reset_index(drop=True)
-
+def add_BOS_CHoCH(fig):
     bos_data = smc.bos_choch(df)
 
-    bos = bos_data["BOS"]
-    choch = bos_data["CHOCH"]
-    direction = bos_data["Direction"]
-    level = bos_data["Level"]
-    broken_index = bos_data["BrokenIndex"]
-
     # plot these lines on a graph
-    fig = go.Figure(data=[go.Candlestick(x=df['date'],
-        open=df['open'], high=df['high'],
-        low=df['low'], close=df['close'])
-    ])
-
-    # plot a line for each bos
-
-    for i in range(len(bos)):
-        if bos[i] == 1:
-            fig.add_trace(go.Scatter(
-                x=[df['date'][i], df['date'][broken_index[i]]],
-                y=[level[i], level[i]],
-                mode="lines",
-                line=dict(
-                    color="rgba(255,0,0,1)"
+    for i in range(len(bos_data["BOS"])):
+        if bos_data["BOS"][i] == 1 or bos_data["CHOCH"][i] == 1:
+            fig.add_trace(
+                go.Scatter(
+                    x=[df["date"][i], df["date"][bos_data["BrokenIndex"][i]]],
+                    y=[bos_data["Level"][i], bos_data["Level"][i]],
+                    mode="lines",
+                    line=dict(
+                        color="red" if bos_data["BOS"][i] == 1 else "green",
+                    ),
+                    opacity=0.5,
                 )
-            ))
-        if choch[i] == 1:
-            # do the same with a different color
-            fig.add_trace(go.Scatter(
-                x=[df['date'][i], df['date'][broken_index[i]]],
-                y=[level[i], level[i]],
-                mode="lines",
-                line=dict(
-                    color="rgba(0,255,0,1)"
-                )
-            ))
+            )
+    return fig
 
-    fig.show()
 
-test_BOS()
-
-def test_OB():
-    df = pd.read_csv('bittrex_btc-usdt.csv')
-    # use the last 200 candles
-    df = df.iloc[-200:]
-    # reset the index
-    df = df.reset_index(drop=True)
-
+def add_OB(fig):
     ob_data = smc.ob(df)
 
-    print(ob_data)
-
-    ob = ob_data["OB"]
-    direction = ob_data["Direction"]
-    top = ob_data["Top"]
-    bottom = ob_data["Bottom"]
-    size = ob_data["Size"]
-    mitigated = ob_data["Mitigated"]
-    mitigated_index = ob_data["MitigatedIndex"]
-
-    fig = go.Figure(data=[go.Candlestick(x=df['date'],
-        open=df['open'], high=df['high'],
-        low=df['low'], close=df['close'])
-    ])
-
     # plot the same way as FVG
-    for i in range(len(ob)):
-        if ob[i] == 1:
-            if mitigated[i] == 1:
-                fig.add_shape(
-                    # unfilled Rectangle
-                    type="rect",
-                    x0=df['date'][i],
-                    y0=top[i],
-                    x1=df['date'][mitigated_index[i]],
-                    y1=bottom[i],
+    for i in range(len(ob_data["OB"])):
+        if ob_data["OB"][i] == 1:
+            x1 = (
+                ob_data["MitigatedIndex"][i]
+                if ob_data["Mitigated"][i] == 1
+                else len(df) - 1
+            )
+            fig.add_shape(
+                type="rect",
+                x0=df["date"][i],
+                y0=ob_data["Top"][i],
+                x1=df["date"][x1],
+                y1=ob_data["Bottom"][i],
+                line=dict(
+                    width=0,
+                ),
+                fillcolor="purple",
+                opacity=0.5,
+            )
+    return fig
+
+
+def add_liquidity(fig):
+    liquidity_data = smc.liquidity(df)
+
+    # draw a line horizontally for each liquidity level
+    for i in range(len(liquidity_data["Liquidity"])):
+        if liquidity_data["Liquidity"][i] == 1:
+            fig.add_trace(
+                go.Scatter(
+                    x=[df["date"][i], df["date"][liquidity_data["End"][i]]],
+                    y=[liquidity_data["Level"][i], liquidity_data["Level"][i]],
+                    mode="lines",
                     line=dict(
-                        color=("rgba(255,0,0,0.5)" if direction[i] == 1 else "rgba(0,255,0,0.5)"),
-                        width=1,
+                        color="orange",
                     ),
                 )
-            else:
-                fig.add_shape(
-                    # unfilled Rectangle
-                    type="rect",
-                    x0=df['date'][i],
-                    y0=top[i],
-                    x1=df['date'][len(df)-1],
-                    y1=bottom[i],
+            )
+        if liquidity_data["Swept"][i] != 0:
+            # draw a red line between the end and the swept point
+            fig.add_trace(
+                go.Scatter(
+                    x=[
+                        df["date"][liquidity_data["End"][i]],
+                        df["date"][liquidity_data["Swept"][i]],
+                    ],
+                    y=[
+                        liquidity_data["Level"][i],
+                        (
+                            df["high"][liquidity_data["Swept"][i]]
+                            if liquidity_data["BuySellSide"][i] == 2
+                            else df["low"][liquidity_data["Swept"][i]]
+                        ),
+                    ],
+                    mode="lines",
                     line=dict(
-                        color=("rgba(255,0,0,0.5)" if direction[i] == 1 else "rgba(0,255,0,0.5)"),
-                        width=1,
+                        color="red",
                     ),
                 )
+            )
+    return fig
 
-    fig.show()
 
-test_OB()
-
-        
+fig = add_FVG(fig)
+fig = add_highs_lows(fig)
+fig = add_BOS_CHoCH(fig)
+fig = add_OB(fig)
+fig = add_liquidity(fig)
+fig.show()
