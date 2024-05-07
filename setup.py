f24cfbc43dfd290b68ee6472f1d11899dda086c0
Joshua Attridge <joshyattridge@gmail.com>
2024-05-07 20:57:14 +0100

    0.0.19

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import codecs
 import os
 
-VERSION = '0.0.18'
+VERSION = '0.0.19'
 DESCRIPTION = 'Getting indicators based on smart money concepts or ICT'
 
 # read the contents of the README file
