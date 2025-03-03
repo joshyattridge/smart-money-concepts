cf2e127d565b65857264595c05ae4532ea782056
joshyattridge <joshyattridge@gmail.com>
2025-03-03 21:10:34 +0000

    Add a thank you message for users in the initialization script

--- smartmoneyconcepts/__init__.py
+++ smartmoneyconcepts/__init__.py
@@ -1,1 +1,2 @@
 from smartmoneyconcepts.smc import smc
+print("\033[1;33mThank you for using SmartMoneyConcepts! ⭐ Please show your support by giving a star on the GitHub repository: \033[4;34mhttps://github.com/joshyattridge/smart-money-concepts\033[0m")
