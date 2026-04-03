1b62fd6c41e1f508e7ed76831a039fa4c82d42f6
joshyattridge <joshyattridge@gmail.com>
2026-04-03 15:16:50 +0100

    Update version to 0.0.27 in setup.py and smc.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import codecs
 import os
 
-VERSION = "0.0.26"
+VERSION = "0.0.27"
 DESCRIPTION = "Getting indicators based on smart money concepts or ICT"
 
 # read the contents of the README file
--- smartmoneyconcepts/smc.py
+++ smartmoneyconcepts/smc.py
@@ -50,7 +50,7 @@ def apply(decorator):
 
 @apply(inputvalidator(input_="ohlc"))
 class smc:
-    __version__ = "0.0.26"
+    __version__ = "0.0.27"
 
     @classmethod
     def fvg(cls, ohlc: DataFrame, join_consecutive=False) -> Series:
